9f9b1e99b8c7adcc6476d1efde9dce0064cce691
mpdragan <mpdragan@ukr.net>
2021-01-11 20:59:55 +0200

    added all block and footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,15 @@ $(function(){
         slidesToScroll: 1,
     });
 
+    $('.feed-slider__inner-box').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
+
+
 });
